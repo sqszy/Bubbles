9377ccc134d7038082b780bd58050bad8e7f1ef7
coutp <petunyaai@gmail.com>
2023-09-19 15:35:14 +0500

    init

---
+++ BubblesIOS/Bubbles/BubblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblesApp.swift
+//  Bubbles
+//
+//  Created by Петр Михеев on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BubblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
