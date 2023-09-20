60369e83c45659b1e61c882d9337fd8aafc000e9
coutp <petunyaai@gmail.com>
2023-09-20 23:48:41 +0500

    add alembic and made it working

---
+++ fastapi/models/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import (
+    MetaData,
+    Integer,
+    TIMESTAMP,
+    ForeignKey,
+    Table,
+    String,
+    Column,
+    JSON,
+)
+from datetime import datetime
+
+metadata = MetaData()
